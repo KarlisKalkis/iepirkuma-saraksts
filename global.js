81003bfedea76eda4844e0792220c23684115dc3
KarlisKalkis <kkalkis21@gmail.com>
2021-04-20 15:37:46 +0300

    Pievienots produkta ievadisanas lauks

--- global.js
+++ global.js
@@ -1,1 +1,12 @@
-console.log('Javascript works')
+console.log('Javascript works');
+
+document.getElementById("add-product").addEventListener("click", () => {
+    console.log("The product add button is pressed");
+
+    let hided = document.getElementById('hided');
+
+    hided.style.display = 'block'
+})
+
+
+
