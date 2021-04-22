d3578d55c791bd2e7cbc75c48ef5cfdc991bc90c
KarlisKalkis <kkalkis21@gmail.com>
2021-04-22 19:23:53 +0300

    Pievienota constante JS faila

--- global.js
+++ global.js
@@ -1,12 +1,19 @@
+const HIDED = document.getElementById('hided');
+
 console.log('Javascript works');
 
 document.getElementById("add-product").addEventListener("click", () => {
     console.log("The product add button is pressed");
 
-    let hided = document.getElementById('hided');
-
-    hided.style.display = 'block'
+    HIDED.style.display = 'block'
 })
 
 
 
+document.getElementById("confirmProduct").addEventListener("click", () => {
+    HIDED.style.display = 'none'
+    
+
+    console.log("Button Add your product is pressed!");
+
+})
