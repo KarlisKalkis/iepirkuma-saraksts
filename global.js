781fcbd53bddb1a7d48f2c39bd860b9ea3702c43
KarlisKalkis <kkalkis21@gmail.com>
2021-04-22 20:47:57 +0300

    Fiksots ieprieksejo vienibu value nonemsana pec nakamas vienibas ierakstisanas

--- global.js
+++ global.js
@@ -1,18 +1,28 @@
 const HIDED = document.getElementById('hided');
+let hided = [];
 
 console.log('Javascript works');
 
 document.getElementById("add-product").addEventListener("click", () => {
     console.log("The product add button is pressed");
 
-    HIDED.style.display = 'block'
+    HIDED.style.display = 'block';
 })
 
 
 
 document.getElementById("confirmProduct").addEventListener("click", () => {
-    HIDED.style.display = 'none'
+    HIDED.style.display = 'none';
     
+    let PRODUCT = {product: product.value, amount: amount.value, price: price.value};
+
+    product.value = "";
+    amount.value = "";
+    price.value = "";
+
+    hided.push(PRODUCT);
+
+    console.log(PRODUCT);
 
     console.log("Button Add your product is pressed!");
 
