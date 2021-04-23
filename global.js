d895c247da2012e828bf19f93cb5157f55b67332
KarlisKalkis <kkalkis21@gmail.com>
2021-04-23 09:39:15 +0300

    Fiksotas kludas ar neparadisanos ekrana

--- global.js
+++ global.js
@@ -1,11 +1,14 @@
 const HIDED = document.getElementById('hided');
 let hided = [];
 
-console.log('Javascript works');
+window.addEventListener('load', () => {
+    hided = JSON.parse(localStorage.getItem("hided") || "[]");
+    console.log(hided)
+    render();
+})
+
 
 document.getElementById("add-product").addEventListener("click", () => {
-    console.log("The product add button is pressed");
-
     HIDED.style.display = 'block';
 })
 
@@ -14,16 +17,29 @@ document.getElementById("add-product").addEventListener("click", () => {
 document.getElementById("confirmProduct").addEventListener("click", () => {
     HIDED.style.display = 'none';
     
-    let PRODUCT = {product: product.value, amount: amount.value, price: price.value};
+    let PRODUCT = {product: product.value, amount: amount.value, };
 
     product.value = "";
     amount.value = "";
-    price.value = "";
 
     hided.push(PRODUCT);
 
-    console.log(PRODUCT);
-
-    console.log("Button Add your product is pressed!");
-
+    render();
 })
+
+function render() {
+    let saraksts = document.getElementById('saraksts');
+    saraksts.innerHTML = "";
+
+    for(let i = 0; 1 < hided.length; i++) {
+        let PRODUCT = `
+        <div class= "PRODUCT">
+            <h3>Product: ${hided[i].product}</h3>
+            <h4>Amount: ${hided[i].amount}</h4>
+        </div>`
+
+        saraksts.innerHTML += PRODUCT;
+    }
+
+    localStorage.setItem("hided", JSON.stringify(hided))
+}
